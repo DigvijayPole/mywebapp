62f8dd68130884ef81b4c2a25524b3bee6a4f517
DigvijayPole <158149139+DigvijayPole@users.noreply.github.com>
2024-01-31 14:21:08 +0530

    Delete BackendProject directory

--- BackendProject/src/main/java/com/example/BackendProject/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BackendProject.Repository;
-
-import com.example.BackendProject.Entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity,Integer> {
-}
